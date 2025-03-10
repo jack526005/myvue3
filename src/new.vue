f49ddfff33cdf3a7df1b162cc8451ac20e4d7068
toms <3451853256@qq.com>
2025-03-10 18:46:04 +0800

    message

---
+++ src/new.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Hello, Vue 3!{{ h }}:{{ timer }}:{{ count }}</h1>
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const timer =ref(0)
+const h=ref(0)
+
+    setInterval(() => {
+    count.value++
+    if (count.value > 60) {
+        count.value = 0
+        timer.value++
+        if (timer.value > 60) {
+            timer.value = 0
+            h.value++
+            if (h.value > 24) {
+                h.value = 0
+            }
+        }
+    }
+}, 1000)
+    
+
+
+
+
+
+
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
