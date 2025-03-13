a75e4ab9fdcc8a9ee73ff17a76992c492bb15009
toms <3451853256@qq.com>
2025-03-13 15:53:10 +0800

    message

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 //import App from './App.vue'
-import App from './pages/computed.vue'
+import App from './pages/watch.vue'
 createApp(App).mount('#app')
---
+++ src/pages/watch.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="text" v-model="message" >
+    <p>{{ message }}</p>
+
+</template>
+
+<script setup>
+import {ref,watch} from 'vue'
+let message=ref('请输入内容')
+watch(message,(newVal,oldVal)=>{//对message进行监听
+    return console.log(newVal,oldVal)
+})
+window.message=message
+</script>
+
+<style>
+
+</style>
