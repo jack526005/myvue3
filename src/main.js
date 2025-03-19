8ac111167edb9459bf60ed056a6103f006015c8d
toms <3451853256@qq.com>
2025-03-19 20:12:55 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 //import App from './App.vue'
-import App from './pages/class-bind.vue'
+import App from './pages/v-for.vue'
 createApp(App).mount('#app')
+//import { createApp } from 'vue';
+//import ElementPlus from 'element-plus';
+//import 'element-plus/dist/index.css';
+//import App from './App.vue';
+//const app = createApp(App);
+//app.use(ElementPlus);
+//app.mount('#app');
---
+++ src/pages/v-for.vue
@@ -0,0 +1,15 @@
+<template>
+<p v-for="item in obj">{{ item.name }}{{ item.age }}</p>
+<el-button type="warn">Primary Button</el-button>
+</template>
+<script setup>
+import { ref, reactive } from 'vue';
+let list = reactive([1, 2, 3, 4, 5]);
+let arr = reactive(['苹果', '香蕉', '橘子']);
+let obj = reactive([{ name: '张三', age: 18 },{ name: '李四', age: 20 },{ name: '王五', age: 22 }]);
+
+</script>
+
+<style>
+
+</style>
