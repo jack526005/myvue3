1df92fd840b20b5c5247cfd1300992d8750b43a5
jack526005 <149949283+jack526005@users.noreply.github.com>
2025-04-06 16:18:05 +0800

    Add files via upload

---
+++ src/pages/组件父.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import BlogPost from './BlogPost.vue'
+  
+const posts = ref([
+  { id: 1, title: 'My journey with Vue' ,num:3},
+  { id: 2, title: 'Blogging with Vue'},
+  { id: 3, title: 'Why Vue is so fun',num:1}
+])
+</script>
+
+<template>
+	<BlogPost
+  	v-for="post in posts"
+	  :key="post.id"
+  	:title="post.title"
+		:num="post.num"
+	></BlogPost>
+</template>
